8ad939b854c71f66025add94e7c73efe5cbf84c6
Adhil <adhil.muhammed@nergyindia.com>
2024-09-21 01:41:39 +0530

    install react-router-dom to navigation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Master } from "master";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,7 +16,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Master />
+      <BrowserRouter>
+        <Master />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
--- src/master/Master.js
+++ src/master/Master.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 export const Master = () => {
-  return <div>Master</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<>Master</>} />
+    </Routes>
+  );
 };
