71017fa2274285d1d8cdd6e2d341874f4b0a1891
Adhil <adhil.muhammed@nergyindia.com>
2024-09-21 00:36:19 +0530

    install neccessary packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Master } from "master";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: false, refetchOnWindowFocus: false, staleTime: Infinity },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Master />
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/master/Master.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Master = () => {
+  return <div>Master</div>;
+};
