33b727792e65838a7547b1b39406a84a6ea3e6c5
Adhil <adhil.muhammed@nergyindia.com>
2024-09-23 00:55:18 +0530

    ui improvement

--- src/modules/Home/components/Slider.jsx
+++ src/modules/Home/components/Slider.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa"; // Import icons
+import { useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 export const Slider = () => {
+  const countryState = useSelector((state) => state?.country);
   const [currentIndex, setCurrentIndex] = React.useState(0);
 
+  console.log("countryState?.data: ", countryState?.data);
+
   const slides = [
-    { id: 1, url: "https://via.placeholder.com/600x300?text=Slide+1" },
+    { id: 1, url: "https://flagcdn.com/ax.svg" },
     { id: 2, url: "https://via.placeholder.com/600x300?text=Slide+2" },
     { id: 3, url: "https://via.placeholder.com/600x300?text=Slide+3" },
     { id: 4, url: "https://via.placeholder.com/600x300?text=Slide+4" },
@@ -29,30 +34,61 @@ export const Slider = () => {
 
   return (
     <>
-      <div className="line-with-label home w-45 my-4">
+      <div className="line-with-label home w-100 mb-4">
         <span className="fs-1" style={{ fontWeight: 600 }}>
           Welcome
         </span>
       </div>
-      <div className="slider-container">
-        <div className="slide" key={slides[currentIndex].id}>
+      <Row>
+        <Col sm={12} md={10}>
+          <div className="slider-container">
+            <div
+              className="slide d-flex justify-content-center"
+              key={slides[currentIndex].id}
+            >
+              <span className="slider-name">
+                {countryState?.data[currentIndex + 5]?.name}
+              </span>
+              <img
+                src={countryState?.data[currentIndex + 5]?.flag}
+                alt={`Slide ${currentIndex + 1}`}
+              />
+            </div>
+            <div>
+              <FaArrowLeft
+                size={35}
+                className="slider-arrow left-arrow"
+                onClick={prevSlide}
+              />
+              <div></div>
+              <FaArrowRight
+                size={35}
+                className="slider-arrow  right-arrow"
+                onClick={nextSlide}
+              />
+            </div>
+
+            <div className="dots">
+              {countryState?.data.slice(0, 4)?.map((slide, index) => (
+                <span
+                  key={slide.id}
+                  className={`dot ${
+                    currentIndex === index ? "active-slide" : ""
+                  }`}
+                  onClick={() => goToSlide(index)}
+                ></span>
+              ))}
+            </div>
+          </div>
+        </Col>
+        <Col sm={12} md={2}>
           <img
-            src={slides[currentIndex].url}
+            src={countryState?.data[40]?.flag}
             alt={`Slide ${currentIndex + 1}`}
+            className="img-fluid"
           />
-        </div>
-        <FaArrowLeft className="prev" size={35} onClick={prevSlide} />
-        <FaArrowRight className="next" size={35} onClick={nextSlide} />
-        <div className="dots">
-          {slides.map((slide, index) => (
-            <span
-              key={slide.id}
-              className={`dot ${currentIndex === index ? "active-slide" : ""}`}
-              onClick={() => goToSlide(index)}
-            ></span>
-          ))}
-        </div>
-      </div>
+        </Col>
+      </Row>
     </>
   );
 };
